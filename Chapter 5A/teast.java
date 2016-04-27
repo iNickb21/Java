d5981343416d4a03054a9bfd9352bf909724b59a
Nick Boyett <inickb21@gmail.com>
2016-04-27 08:28:56 -0400

    Removed non assignment program

--- Chapter 5A/teast.java
+++
@@ -1,9 +0,0 @@
-public class teast
-{
-    public static void main(String[] args)
-    {
-        StringBuffer str = new StringBuffer("abcdefg");
-        str.insert(2,"XXX");
-        System.out.println(str);
-    }
-}
